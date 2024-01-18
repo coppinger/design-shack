604afd0cf6d288bddcb178cdc05e8413fe42e136
Charlie Coppinger <charles.coppinger@gmail.com>
2024-01-19 10:44:25 +1300

    capital B

--- src/components/site/PageHome.tsx
+++ src/components/site/PageHome.tsx
@@ -4,7 +4,7 @@ export async function PageHome() {
   return (
     <iframe
       src="https://build-in-public-network.vercel.app/admin/tags"
-      frameborder="0"
+      frameBorder="0"
       style={{ width: "100vw", height: "100vh", pointerEvents: "none" }}
     ></iframe>
   );
